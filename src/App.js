658b91f0f1d17002e2296631e6d29b0859a8f54c
Tanisha Joshi <tanishajoshi.guitar@gmail.com>
2022-04-08 15:29:55 -0500

    fix routing

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ export default function App() {
       <Routes>
         {getRoutes(routes)}
         <Route path="/" element={<Presentation />} />
-        <Route path="*" element={<Navigate to="/my-spa" />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </ThemeProvider>
   );
